6f2eca64f76463a23ca63c5aa089940a9358f09e
Raphaël Claasen <raphael.claasen@gmail.com>
2020-09-02 18:02:09 +0200

    Add stringToTime() to helpers

--- src/timeHelpers.test.ts
+++ src/timeHelpers.test.ts
@@ -1,52 +1,53 @@
-import { absSecondsLeft, absMinutesLeft, timeToDegrees } from './timeHelpers'
+import { millisecondsToAbsSeconds, millisecondsToAbsMinutes, timeToDegrees, 
+         stringToTime } from './timeHelpers'
 
-describe('absSecondsLeft()', () => {
+describe('millisecondsToAbsSeconds()', () => {
   it('returns 0 when passed 0', () => {
-    expect(absSecondsLeft(0)).toEqual(0);
+    expect(millisecondsToAbsSeconds(0)).toEqual(0);
   });
   
   it('return 1 when passed 1000', () => {
-    expect(absSecondsLeft(1000)).toEqual(1);
+    expect(millisecondsToAbsSeconds(1000)).toEqual(1);
   });
   
   it('returns 1 when passed -1000', () => {
-    expect(absSecondsLeft(-1000)).toEqual(1);
+    expect(millisecondsToAbsSeconds(-1000)).toEqual(1);
   });
 
   it('returns 0 when passed 1', () => {
-    expect(absSecondsLeft(1)).toEqual(0);
+    expect(millisecondsToAbsSeconds(1)).toEqual(0);
   });
 
   it('returns 0 when passed 999', () => {
-    expect(absSecondsLeft(999)).toEqual(0);
+    expect(millisecondsToAbsSeconds(999)).toEqual(0);
   });
 
   it('returns 0 when passed -500', () => {
-    expect(absSecondsLeft(-500)).toEqual(0);
+    expect(millisecondsToAbsSeconds(-500)).toEqual(0);
   });
 });
 
 
 
-describe('absMinutesLeft()', () => {
+describe('millisecondsToAbsMinutes()', () => {
   it('returns 0 when passed 0', () => {
-    expect(absMinutesLeft(0)).toEqual(0);
+    expect(millisecondsToAbsMinutes(0)).toEqual(0);
   });
 
   it('returns 1 when passed 60000', () => {
-    expect(absMinutesLeft(60000)).toEqual(1);
+    expect(millisecondsToAbsMinutes(60000)).toEqual(1);
   });
 
   it('returns 1 when passed -60000', () => {
-    expect(absMinutesLeft(-60000)).toEqual(1);
+    expect(millisecondsToAbsMinutes(-60000)).toEqual(1);
   });
 
   it('returns 0 when passed 1', () => {
-    expect(absMinutesLeft(1)).toEqual(0);
+    expect(millisecondsToAbsMinutes(1)).toEqual(0);
   });
 
   it('returns 0 when passed 59999', () => {
-    expect(absMinutesLeft(59999)).toEqual(0);
+    expect(millisecondsToAbsMinutes(59999)).toEqual(0);
   });
 });
 
@@ -62,4 +63,42 @@ describe('timeToDegrees()', () => {
   it('returns -90 when passed -15 minutes', () => {
     expect(timeToDegrees(-15 * 60000)).toEqual(-90);
   });
-})
+});
+
+describe('stringToTime()', () => {
+  it('returns 0 when passed an empty string', () => {
+    expect(stringToTime("")).toEqual(0);
+  });
+
+  it('returns 1000 when passed "1"', () => {
+    expect(stringToTime("1")).toEqual(1000);
+  });
+
+  it('returns 60000 when passed "100"', () => {
+    expect(stringToTime("100")).toEqual(60000);
+  });
+
+  it('returns 3600000 when passed "10000"', () => {
+    expect(stringToTime("10000")).toEqual(3600000);
+  });
+
+  it('returns 5025000 when passed "12345"', () => {
+    expect(stringToTime("12345")).toEqual(5025000);
+  });
+
+  it('returns 360000000 when passed "1000000"', () => {
+    expect(stringToTime("1000000")).toEqual(360000000);
+  });
+
+  it('returns 90000 when passed "90"', () => {
+    expect(stringToTime("90")).toEqual(90000);
+  });
+
+  it('returns 5400000 when passed "9000"', () => {
+    expect(stringToTime("9000")).toEqual(5400000);
+  });
+
+  it('returns NaN when passed "12A34"', () => {
+    expect(stringToTime("12A34")).toEqual(NaN);
+  });
+});
--- src/timeHelpers.ts
+++ src/timeHelpers.ts
@@ -1,13 +1,36 @@
-export function absSecondsLeft(timeInMilliseconds: number) {
+export function millisecondsToAbsSeconds(timeInMilliseconds: number) {
   return Math.floor(Math.abs(timeInMilliseconds) / 1000);
 }
 
-export function absMinutesLeft(timeInMilliseconds: number) {
+export function millisecondsToAbsMinutes(timeInMilliseconds: number) {
   return Math.floor(Math.abs(timeInMilliseconds) / 60000);
+}
+
+export function secondsToMilliseconds(timeInSeconds: number) {
+  return timeInSeconds * 1000;
+}
+
+export function minutesToMilliseconds(timeInMinutes: number) {
+  return timeInMinutes * 60000;
 }
 
 // Converts a time (in milliseconds) to the amount of degrees the minute arm
 // of a clock would be rotated from the top.
 export function timeToDegrees(timeInMilliseconds: number) {
   return (timeInMilliseconds / 60000) * 6;
+}
+
+// Converts a string in the format of HHMMSS to an amount milliseconds.
+export function stringToTime(inputString: string) {
+  const inputNumber = Number(inputString);
+
+  const seconds = inputNumber % 100;
+  const minutes = (inputNumber % 10000 - seconds) / 100;
+  const hours = (inputNumber - (minutes * 100) - seconds) / 10000;
+
+  const totalTime = seconds * 1000
+                  + minutes * 60 * 1000
+                  + hours * 60 * 60 * 1000;
+
+  return totalTime;
 }
