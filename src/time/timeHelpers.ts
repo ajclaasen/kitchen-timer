5a6e6350f2f5e37392752950877255309a6389a4
Raphaël Claasen <raphael.claasen@gmail.com>
2020-09-08 01:52:59 +0200

    Add helpers to calculate mins and secs on clock

--- src/time/timeHelpers.ts
+++ src/time/timeHelpers.ts
@@ -38,3 +38,30 @@ export function stringToTime(inputString: string) {
 export function minutesAndSecondsToTime(minutes: number, seconds: number) {
   return minutesToMilliseconds(minutes) + secondsToMilliseconds(seconds);
 }
+
+export function minutesOnClock(milliseconds: number) {
+  // As we are rounding up seconds as well, we should round up a minute during
+  // the last second of a minute.
+  let minutes = millisecondsToAbsMinutes(milliseconds);
+  const withoutMinutes = milliseconds % 60000;
+  if(withoutMinutes > 59000) {
+    minutes++;
+  }
+  return minutes;
+}
+
+export function secondsOnClock(milliseconds: number) {
+  const withoutMinutes = milliseconds % 60000;
+
+  // We do not show any time units smaller than a second, so it is more
+  // intuitive to round up than to round down, so we +1 the rounded down value.
+  let seconds = millisecondsToAbsSeconds(withoutMinutes);
+  if(withoutMinutes % 1000 > 0) {
+    seconds++;
+  }
+  // But 60 seconds should be displayed as a minute instead.
+  if(seconds == 60) {
+    return 0;
+  }
+  return seconds;
+}
